d0f9605eb78feed3e12f54471e3adb0571af597f
Frauches <frauches.27@gmail.com>
2015-05-08 01:26:58 -0300

    AGORA VAI

--- ProjetoNomes/src/util/Principal.java
+++ ProjetoNomes/src/util/Principal.java
@@ -17,8 +17,8 @@ public class Principal
     public static void main (String args [])
     {
         
-        ManipulaArquivo.getInstance().buscaArquivoNome("nomes");
-        ManipulaArquivo.getInstance().buscaArquivoNome("sobrenomes");
+        System.out.println(ManipulaArquivo.getInstance().lerNomes());
+        
         
     }
    
